251fa1b699a1316ceaf6e04fc6d64799da9d8b49
Kazma Lomlice <komiya830124@gmail.com>
2021-02-14 21:04:30 +0800

    -first commit

---
+++ src/dto/TodayWeather.ts
@@ -0,0 +1,10 @@
+export interface Weather {
+    feelingTemperature?: string;
+    highTemperature?: string;
+    lowTemperature?: string;
+    humidity?: string;
+    UVLevel?: string;
+    airQualityValue?: string;
+    airQualityCategory?: string
+    airQualityDescription?: string;
+}
---
+++ src/requestModel.d.ts
@@ -0,0 +1,3 @@
+interface objectWeather{
+    children:Array
+}
