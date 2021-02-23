382fac9ee938dacd3713a29b2f16c82569a47260
Kazma Lomlice <komiya830124@gmail.com>
2021-02-23 22:26:33 +0800

    基礎列出指令表完成

--- src/discords/AppDiscord.ts
+++ src/discords/AppDiscord.ts
@@ -1,14 +1,26 @@
-import { ArgsOf, Command, CommandMessage, CommandNotFound, Discord, Guard, On } from "@typeit/discord";
+import { ArgsOf, Client, Command, CommandInfos, CommandMessage, CommandNotFound, Description, Discord, ExpressionFunction, Guard, Infos, On, RuleBuilder } from "@typeit/discord";
 import * as Path from 'path'
-const discordRegex =new RegExp("(?=Cier |cier |!).*")
+const discordRegex = new RegExp("(?=Cier |cier |!).*")
 // @Discord(discordRegex,{
 // import:[
 //     Path.join
 // ]
 // })
+
 @Discord(discordRegex)
+@Description("Admin commands")
 export abstract class AppDiscord {
-    
+
+    @Command("help")
+    help(message: CommandMessage) {
+
+        // 取得目前injected的command
+        let commandList: Array<CommandInfos> = Client.getCommands()
+
+
+        message.reply("你可以使用一些命令如下:\n" + commandList.map(e=>e.commandName ))
+        console.log(commandList)
+    }
     @Command("hello")
     hello(message: CommandMessage) {
         message.reply("HI!")
@@ -25,6 +37,6 @@ export abstract class AppDiscord {
         message = message
         //FIXME remove the call inst. which in message
         message.reply(`別傻了，我沒有${message.content}...`)
-        
+
     }
 }
