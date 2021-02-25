359df118c2ad2017d83c39128974e81d2be9288f
Kazma Lomlice <komiya830124@gmail.com>
2021-02-25 09:43:53 +0800

    help command done

--- src/discords/commands/Help.ts
+++ src/discords/commands/Help.ts
@@ -1,16 +1,19 @@
-import { Client, Command, CommandInfos, CommandMessage } from "@typeit/discord";
+import { Client, Command, CommandInfos, CommandMessage, Infos } from "@typeit/discord";
 
 
 export abstract class Help {
     @Command("help")
+    @Infos({ description: "What commands you can use." })
     async help(command: CommandMessage) {
         //取得目前injected的command
         let commandList: Array<CommandInfos> = Client.getCommands()
-
-
-        command.reply("你可以使用一些命令如下:\n" + commandList.map(e => {
-            return  `${e.commandName}\tinfo: ${e.infos}\n`
-        }))
+        const commandHelp = commandList.map(eachCommand => {
+            return `${eachCommand.commandName}\t${eachCommand.description}`
+        })
+        
+        command.reply("你可以使用一些命令如下:\n" + commandHelp.join("\n")
+        
+        )
         console.log(commandList)
     }
-}
+}   
